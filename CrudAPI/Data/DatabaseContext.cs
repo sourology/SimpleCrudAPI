5129c1acc8aa86da543b0bfb37e8e11fae830a18
Sourov Halder <112756349+sourology@users.noreply.github.com>
2022-09-08 00:51:23 +0600

    Add database context

---
+++ CrudAPI/Data/DatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using CrudAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudAPI.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+        public DbSet<Book> Books { get; set; } = default!;
+    }
+}
