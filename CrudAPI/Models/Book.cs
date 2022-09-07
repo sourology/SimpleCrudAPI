6aecd48dcc589805dd85372f569f630fa7ddf8f5
Sourov Halder <112756349+sourology@users.noreply.github.com>
2022-09-08 00:50:59 +0600

    Add a model
    Book.cs

---
+++ CrudAPI/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudAPI.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Author { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+        public string? BookImage { get; set; }
+    }
+}
